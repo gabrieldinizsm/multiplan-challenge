ca8293e7cd0aaa998814d97ce823689e25d96f91
dinizgabriel <gabrieldinizsm@hotmail.com>
2025-08-17 17:15:40 -0300

    feat(write_dataframe_output): Criando function pra gerir output dos dataframes

--- app/io_utils.py
+++ app/io_utils.py
@@ -11,3 +11,20 @@ def ensure_output_dir_exists(path: Path) -> None:
     path.mkdir(parents=True, exist_ok=True)
 
 
+def write_dataframe_output(df: pl.DataFrame, path: Path, format: str = "json") -> None:
+    """
+    Escreve o DataFrame para o path desejado no formato desejado.
+
+    Args:
+        df (pl.DataFrame): DataFrame a ser escrito.
+        path (Path): Path do local e nome a ser salvo.
+        format (str): Formato do arquivo, um dos três (csv, json e parquet).
+    """
+    if format == "csv":
+        df.write_csv(path)
+    elif format == "json":
+        df.write_json(path)
+    elif format == "parquet":
+        df.write_parquet(path)
+    else:
+        raise ValueError(f"Formato não suportado: {format}")
