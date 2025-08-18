a9ca558cf28a4dc5dba74a46ad7a97c0a7a8c523
dinizgabriel <gabrieldinizsm@hotmail.com>
2025-08-17 22:27:11 -0300

    tests(test_filter_top_n_by_response_time): Adicionando testes unitários

---
+++ tests/test_transformation.py
@@ -0,0 +1,24 @@
+import polars as pl
+import app.transformation as tf
+
+def test_filter_top_n_by_response_time():
+    target_referrer = "http://xpto.com"
+
+    data = {
+        "request": ["GET /manual/page1", "GET /manual/page2", "POST /manual/page3", "GET /other/page"],
+        "statusCode": [200, 200, 400, 200],
+        "referrerHeader": [target_referrer, target_referrer, target_referrer, target_referrer],
+        "responseTime": [100, 300, 200, 400]
+    }
+
+    df = pl.DataFrame(data)
+
+    top_df = tf.filter_top_n_by_response_time(df, target_referrer=target_referrer, top_n=2)
+
+    assert top_df.shape[0] == 2
+    assert top_df["responseTime"].to_list() == [300, 100]  
+
+    assert all(top_df["request"].to_list()[i].startswith("GET /manual/") for i in range(top_df.shape[0]))
+    assert all(top_df["statusCode"] == 200)
+    assert all(top_df["referrerHeader"] == target_referrer)
+
