970be0884296829ed8b7c74a56c1e55ff891d404
dinizgabriel <gabrieldinizsm@hotmail.com>
2025-08-18 00:19:13 -0300

    tests(test_write_dataframe_output_json): Adicionando testes unitários na camada io_utils

--- tests/test_io_utils.py
+++ tests/test_io_utils.py
@@ -10,10 +10,27 @@ def test_write_dataframe_output_csv(tmp_path):
     })
     file_path = tmp_path / "test.csv"
 
-    print(df.head())
-
     io_utils.write_dataframe_output(df, file_path, "csv")
 
     df_read = pl.read_csv(file_path)
 
+    assert file_path.exists()
     assert df_read.equals(df)
+
+
+def test_test_write_dataframe_output_json(tmp_path):
+    df = pl.DataFrame({
+        "col1": [1, 2],
+        "col2": ["x", "y"]
+    })
+    file_path = tmp_path / "test.json"
+
+    io_utils.write_dataframe_output(df, file_path, "json")
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    expected = [{"col1": 1, "col2": "x"}, {"col1": 2, "col2": "y"}]
+
+    assert file_path.exists()
+    assert data == expected
